8bffad5bf0361f47432a70aeb5d4ea691691467e
Pushpa Gnyawali <75498254+Pushpa88431@users.noreply.github.com>
2024-02-19 17:39:27 +0200

    header component added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,22 @@
-function Header() {
-    return ( 
-        <header></header>
-     );
-}
+import React, { useState } from "react";
+import imageUrl from "./assets/6.png";
+const Header: React.FC = () => {
+  const [search, setSearch] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  return (
+    <header>
+      <div>
+        <img src={imageUrl} height="60px;" width="60px;" alt="Logo" />
+
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => setSearch(e.target.value)}
+        />
+      </div>
+    </header>
+  );
+};
 
 export default Header;
