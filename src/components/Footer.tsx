115b90abc7c00f481656a7f65fdc6b979d6672fc
Pushpa Gnyawali <75498254+Pushpa88431@users.noreply.github.com>
2024-02-19 17:48:08 +0200

    footer component added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,28 @@
-function Footer() {
-    return ( 
-        <footer></footer>
-     );
-}
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faTwitter,
+  faFacebook,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
+import imageUrl from "./assets/6.png";
+const Footer: React.FC = () => {
+  return (
+    <footer>
+      <div>
+        <img src={imageUrl} height="60px;" width="60px;" alt="Logo" />
+        <a href="https://twitter.com">
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+        <a href="https://facebook.com">
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+        <a href="https://instagram.com">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
