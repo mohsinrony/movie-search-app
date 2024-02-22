dd0c3e8010a8c525a18b0901e5085590d3a86fde
Jubair <me.jubair@yahoo.com>
2024-02-22 20:55:26 +0200

    Set up routing

--- src/components/MovieList.tsx
+++ src/components/MovieList.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import '../styles/MovieList.css';
+
 
 interface Movie {
   id: number; 
@@ -15,20 +16,28 @@ interface MovieListProps {
 }
 
 const MovieList: React.FC<MovieListProps> = ({ movies }) => {
+  useEffect(() => {
+    document.title = 'Movies | T3 MovieDB App';
+  }, []);
+
   return (
     <div>
       <h1>Movie List</h1>
       <div className="movie-list">
-        {movies.map(movie => (
-          <div key={movie.id} className="movie-item">
-            <Link to={`/movies/${movie.id}`}>
-              {movie.poster && <img src={movie.poster} alt={movie.name} />}
-              <h2>{movie.name}</h2>
-              <p>{movie.year}</p>
-              <p>{movie.rating}</p>
-            </Link>
-          </div>
-        ))}
+        {movies.length === 0 ? (
+          <h2>No Movies Found</h2>
+        ) : (
+          movies.map(movie => (
+            <div key={movie.id} className="movie-item">
+              <Link to={`/movies/${movie.id}`}>
+                {movie.poster && <img src={movie.poster} alt={movie.name} />}
+                <h2>{movie.name}</h2>
+                <p>{movie.year}</p>
+                <p>{movie.rating}</p>
+              </Link>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
